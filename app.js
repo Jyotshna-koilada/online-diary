44d0de6108563159d73d4c0e1cb5f43ef4a76581
Jyotshna-koilada <yernirupika@gmail.com>
2021-07-06 23:30:59 +0530

    Final website

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const getDate = require(__dirname + '/public/js/date.js');
-const bcrypt = require('bcrypt');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
